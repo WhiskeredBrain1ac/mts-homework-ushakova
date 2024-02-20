891604a21560c507ec46cc35befe434789aafda6
WhiskeredBrain1ac <tech.profile.acc@gmail.com>
2024-02-20 22:48:28 +0300

    First

--- hws/src/main/java/ru/ushakova/java/hws/Application.java
+++ hws/src/main/java/ru/ushakova/java/hws/Application.java
@@ -1,7 +1,13 @@
 package ru.ushakova.java.hws;
 
+
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-
+        System.out.println("Введите Ваше имя: ");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        System.out.println("Привет, " + name + "!");
     }
 }
